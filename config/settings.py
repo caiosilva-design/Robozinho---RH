5d2392c7ca615d361beb761d2d932c4450b9c3fd
caiosilva-design <caiodasilvacaio@gmail.com>
2026-07-19 23:50:28 -0300

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#LOGIN = os.getenv("LOGIN")
+#SENHA = os.getenv("SENHA")
+
+# URL de cancelamento...
+URL_LOGIN = os.getenv("URL_LOGIN")
+URL_HOME = os.getenv("URL_HOME")
+
+# URL de abertura...
+URL_LOGIN2 = os.getenv("URL_LOGIN2")
